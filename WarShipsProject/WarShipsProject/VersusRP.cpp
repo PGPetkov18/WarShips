1f278e05da586512b642d29129753b3d09c02ba5
dpdyakov18 <dpdyakov18@codingburgas.bg>
2021-01-11 22:53:30 +0200

    Fixed some bugs connected with the menus repeating and counting the right guessed number

--- WarShipsProject/WarShipsProject/VersusRP.cpp
+++ WarShipsProject/WarShipsProject/VersusRP.cpp
@@ -2,17 +2,91 @@
 #include <iostream>
 using namespace std;
 
-int GermansNumbers[5];
-int BreakersGuessNumbersRP[5];
-int RightGuessedNumberRP[5];
+int GermansNumbers[4];
+int BreakersGuessNumbersRP[4];
+int RightGuessedNumberRP[4];
 int NumbersCountRP = 0;
 int CoordsCountRP = 0;
 bool flagRP=false;
+bool flagCheckRPN = false;
+int HistoryOfRoundsRP[14][5];
+int RowCounterRP = 1;
+
+void StoringHistoryOfRoundsRP()
+{
+	for (int j = 0; j < 4; j++)
+	{
+		HistoryOfRoundsRP[RowCounterRP][j] = BreakersGuessNumbersRP[j];
+	}
+	RowCounterRP++;
+}
+
+void VisualizingHistoryOfRoundsRP()
+{
+	for (int i = 0; i < RowCounterRP; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			cout << "[ ";
+			cout << HistoryOfRoundsRP[i][j] << " ";
+			cout << "] ";
+		}
+		cout << endl;
+	}
+}
+
+void CheckGermansRPN()
+{
+	for (int i = 0; i <= 4; i++)
+	{
+		for (int j = i; j <= 4; j++)
+		{
+			do
+			{
+				if (GermansNumbers[i] == GermansNumbers[j])
+				{
+					flagCheckRPN = true;
+				}
+				if (flagCheckRPN == true)
+				{
+					cout << "Please type the number again" << i + 1 << ":" << endl;
+					flagCheckRPN = false;
+					cin >> GermansNumbers[i];
+				}
+			} while (flagCheckRPN == true);
+			flagCheckRPN = false;
+		}
+	}
+}
+
+void CheckBreakersRPN()
+{
+	for (int i = 0; i <= 4; i++)
+	{
+		for (int j = i; j <= 4; j++)
+		{
+			do
+			{
+				if (BreakersGuessNumbersRP[i] == BreakersGuessNumbersRP[j])
+				{
+					flagCheckRPN = true;
+				}
+				if (flagCheckRPN == true)
+				{
+					cout << "Please type the number again" << i + 1 << ":" << endl;
+					flagCheckRPN = false;
+					cin >> BreakersGuessNumbersRP[i];
+				}
+			} while (flagCheckRPN == true);
+			flagCheckRPN = false;
+		}
+	}
+}
 
 void InputGermansNumbersR()
 {
 	cout << "Type 4 numbers between 0 and 7!" << endl;
-	for (int i = 1; i <= 4; i++)
+	for (int i = 0; i < 4; i++)
 	{
 		cout << "Number " << i << ":";
 		cin >> GermansNumbers[i];
@@ -28,10 +102,11 @@ void InputGermansNumbersR()
 void InputGermansNumbersNR()
 {
 	cout << "Type 4 numbers between 0 and 7 without repeating any of them!" << endl;
-	for (int i = 1; i <= 4; i++)
+	for (int i = 0; i < 4; i++)
 	{
 		cout << "Number " << i << ":";
 		cin >> GermansNumbers[i];
+		CheckGermansRPN();
 		if (GermansNumbers[i] < 0 || GermansNumbers[i]>7)
 		{
 			cout << "Please, type the number again!";
@@ -44,19 +119,20 @@ void InputGermansNumbersNR()
 
 void InputBreakersGuess()
 {
-	cout << "Please, enter the numbers you want to guess and the coordinate where you think they are: " << endl;
-	for (int i = 1; i <= 4; i++)
+	cout << "Please, enter the numbers you want to guess: " << endl;
+	for (int i = 0; i < 4; i++)
 	{
 		cout << "Number " << i << ": ";
 		cin >> BreakersGuessNumbersRP[i];
+		CheckBreakersRPN();
 	}
 }
 
 void CheckBreakersGuessRP()
 {
-	for (int i = 1; i <= 4; i++)
+	for (int i = 0; i < 4; i++)
 	{
-		for (int j = 1; j <= 4; j++)
+		for (int j = 0; j < 4; j++)
 		{
 			if (BreakersGuessNumbersRP[i] == GermansNumbers[j])
 			{
@@ -65,7 +141,6 @@ void CheckBreakersGuessRP()
 				{
 					CoordsCountRP++;
 				}
-				i++;
 			}
 		}
 	}
@@ -75,12 +150,12 @@ void FeedbackAfterRound()
 {
 	if (CoordsCountRP == 0)
 	{
-		cout << "You've guessed " << NumbersCountRP << "numbers correctly, but none of them was in the right spot!" << endl;
+		cout << "You've guessed " << NumbersCountRP << " numbers correctly, but none of them was in the right spot!" << endl;
 		NumbersCountRP = 0;
 	}
 	else if(CoordsCountRP!=4 && CoordsCountRP!=0)
 	{
-		cout << "You've guessed " << CoordsCountRP+1 << "numbers correctly on their places, and " << NumbersCountRP-CoordsCountRP << "numbers, but not on their correct places!" << endl;
+		cout << "You've guessed " << CoordsCountRP << " numbers correctly on their places, and " << NumbersCountRP-CoordsCountRP << "numbers, but not on their correct places!" << endl;
 		CoordsCountRP = 0;
 		NumbersCountRP = 0;
 	}
